6e9b6b2b9fa0f6926b40fcc89f7afc957a24cd1d
corynissen <corynissen@gmail.com>
2013-08-26 10:51:51 -0500

    first commit

---
+++ update_LA_fp_data.R
@@ -0,0 +1,35 @@
+
+source("dynamo_scripts.R")
+
+table_name <- "cory_tweets"
+table_search_term <- "la_food_poisoning"
+search_term <- "food poisoning"
+geocode <- "33.9999,-118.392,23mi"
+
+################################################################################
+# Run here...
+
+# what this is supposed to do...
+# 1. query dynamodb for the most recent tweetid with the hash_key of search_term
+# 2. query twitter with a sinceid that was returned from the last step
+# 3. upload the new tweets to dynamo
+
+# step #1
+tweetid.query <- getLastTweet(table_name, table_search_term)
+if(grepl("'Count': 1", tweetid.query)){
+  # get last.tweetid, use grep instead of fromJSON so we don't have to worry
+  # about malformed JSON
+  last.tweetid <- substring(tweetid.query,
+                            regexpr("'RangeKeyElement':", tweetid.query))
+  last.tweetid <- substring(last.tweetid, regexpr(":", last.tweetid)+2,
+                            regexpr(",", last.tweetid)-1)
+}else{
+  last.tweetid <- ""
+}
+
+# step #2
+tweets.json <- twitter_search(term=search_term, count=100, geocode=geocode,
+                              since_id=last.tweetid)
+tweets <- fromJSON(tweets.json, asText=TRUE)
+# step #3
+lapply(tweets$statuses, function(x)uploadTweet(x, table_name, table_search_term))
